8148e0e041fbcecb0a0dfd38c0b7b877aedad1b2
guillaume <guillaume.chausse@live.fr>
2017-08-29 13:48:30 +0200

    Initial commit

---
+++ montheme/footer.php
@@ -0,0 +1,21 @@
+<footer>
+            <div>
+                <section>
+                    <h4>follow me!</h4>
+                    <p>ouhveurohv sdufhvuehrp hgvnov dopv poeudhvner n vehr uhuerç qpfpbq vb!</p>
+                </section>
+                <section>
+                    <h4>flatfy theme</h4>
+                    <ul>
+                        <li>12 column grid bootstrap</li>
+                        <li>form contact</li>
+                        <li>drag gallery</li>
+                        <li>full responsive</li>
+                        <li>lorem impsum</li>
+                    </ul>
+                </section>
+            </div>
+        </footer>
+    </main>
+</body>
+</html>
---
+++ montheme/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+    <h1>flatfy</h1>
+        <h3>clean & minimal theme</h3>
+        <button>Twitter</button>
+        <button>Free Download</button>
+        <img src="../image/circle.png" alt="">
+        <nav id="navbar">
+            <h4>flatfy</h4>
+            <ul>
+                <li>what is?</li>
+                <li>use it</li>
+                <li>screenshot</li>
+                <li>credits</li>
+                <li>contact</li>
+            </ul>
+        </nav>
+    </header>
+    
+</body>
+</html>
